c0704e1157b3a0a4a838d9dad798b660fd21f1a1
Pedro Ivo Silva Brito <pedro.silva@globant.com>
2022-07-04 18:02:40 -0300

    centralized error handling through Query Client's defaultOptions onError prop

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+// import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -13,19 +13,23 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   // search Chakra UI's useToast for better understanding
-  const toast = useCustomToast();
+  // const toast = useCustomToast();
 
   // here we established [] as a default value to be shown while the promise is still pending
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      // setting error title conditionally - if error is an instance of javascript's Error class
-      const title =
-        error instanceof Error
-          ? error.message
-          : 'error connecting to the server';
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+
+  // this excerpt was removed from options (along with all toast logic), for it was not needed once we centralized error handling throught QueryClient default options (queries: {onError})
+  // , {
+  //   onError: (error) => {
+  //     // setting error title conditionally - if error is an instance of javascript's Error class
+  //     const title =
+  //       error instanceof Error
+  //         ? error.message
+  //         : 'error connecting to the server';
+  //     toast({ title, status: 'error' });
+  //   },
+  // }
+
   return data;
 }
--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,18 +1,34 @@
+// here createStandaloneToast is being used because useToast is a hook, and, by the rule of hooks, it can only be used on other hooks or functional components, and this is neither
+import { createStandaloneToast } from '@chakra-ui/react';
 import { QueryClient } from 'react-query';
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+import { theme } from '../theme';
 
-// function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const id = 'react-query-error';
-//   const title =
-//     error instanceof Error ? error.message : 'error connecting to server';
+const toast = createStandaloneToast({ theme });
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const id = 'react-query-error';
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
 
-export const queryClient = new QueryClient();
+  // prevent duplicate toasts
+  toast.closeAll();
+  // needed to add options because cant use customToast here
+  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+}
+
+// here we are centralizing error handling through QueryClient custom options
+// we can still define errorHandler for each useQuery if we want to overwrite the default
+// https://react-query.tanstack.com/reference/QueryClient
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
+
+// Alternatively, we can use React Error Boundary through the usage of defaultOptions' useErrorBoundary
+// if set to true, it will propagate errors to the nearest React Error Boundary instead of handling them from within React Query
+// https://reactjs.org/docs/error-boundaries.html
